72c90d75f1f3967b1597e4d49560f5846ba48bd7
Marius Aruxandei <mariushow@gmail.com>
2022-05-31 11:03:56 +0300

    add random direction after the starting position

--- src/index.js
+++ src/index.js
@@ -56,6 +56,21 @@ World.add(world, walls);
 
 //! Maze generation
 
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+
+  return arr;
+};
+
 const grid = Array(cells)
   .fill(null)
   .map(() => Array(cells).fill(false));
@@ -71,12 +86,32 @@ const horizontals = Array(cells - 1)
 const startRow = Math.floor(Math.random() * cells);
 const startCol = Math.floor(Math.random() * cells);
 
-console.log(startRow, startCol);
-
 const stepThroughCell = (row, column) => {
   // If i have visited the cell at [row. col], return
+  if (grid[row][column] === true) {
+    return;
+  }
   // Mark this cell as being visited
+  grid[row][column] = true;
+
   // Assemble randomly-ordered lsit of neighbors
+
+  const neighbors = shuffle([
+    [startRow - 1, startCol],
+    [startRow, startCol + 1],
+    [startRow + 1, startCol],
+    [startRow, startCol - 1],
+  ]);
+  console.log(neighbors);
+
+  // for (let i = 0; i < neighbors.length; i++) {
+  //   for (let j = 0; j < neighbors[i].length; j++) {
+  //     if (neighbors[i][j] > cells - 1 || neighbors[i][j] < 0) {
+  //       console.log(`Out of bounds at ${neighbors[i][j]}`);
+  //     }
+  //   }
+  // }
+
   // For each neighbor
   // See if that neighbor is out of bounds
   // If we have visited that neighbor, continue to next neighbor
@@ -85,3 +120,4 @@ const stepThroughCell = (row, column) => {
 };
 
 stepThroughCell(startRow, startCol);
+console.log(grid);
